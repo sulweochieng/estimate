1cb2ae07ab585000e2f2b635e6f6dc154221d613
sulweochieng <kodongochieng@students.uonbi.ac.ke>
2023-09-15 11:40:06 +0300

    free'm all

--- memory/free_mem.c
+++ memory/free_mem.c
@@ -3,21 +3,17 @@
 #include "../headers/fprtyps.h"
 
 /**
- * free_linked_list - a function that frees a linked list.
+ * free_list - a function that frees a linked list.
  * @head: pointer to the linked list to be freed.
  */
 void free_list(node *head)
 {
 	node *temp = NULL;
-	int i;
 
-	while ( head != NULL)
+	while (head != NULL)
 	{
 		temp = head;
 		head = head->pointerNext;
-		free(temp->data.name);
-		for (i = 0; i < SALARYLEVELS; i++)
-			free(temp->data.salaryLevels[i].level);
 		free(temp);
 	}
 }
@@ -29,16 +25,12 @@ void free_list(node *head)
 void free_proj(proj *head)
 {
 	proj *temp = NULL;
-	int i;
 
 	while (head != NULL)
 	{
 		temp = head;
 		head = head->pointerNext;
-		free(temp->info.name);
 		free(temp->optLevel);
-		for (i = 0; i < SALARYLEVELS; i++)
-			free(temp->info.salaryLevels[i].level);
 		free(temp);
 	}
 }
